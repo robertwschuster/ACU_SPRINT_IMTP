0616a6e3e2efef92f42ef1b56b1225c51afc48e0
Robert Schuster <robertwschuster@gmail.com>
2022-10-18 13:08:16 +1000

    Init ACU SPRINT IMTP analysis

---
+++ IMTP_analysis.R
@@ -0,0 +1,311 @@
+#-----------------------------------------------------------------------------------------
+#
+# Load and analyze IMTP data (*.csv files)
+# Robert Schuster (ACU SPRINT)
+# July 2022
+#
+#-----------------------------------------------------------------------------------------
+
+
+## clear environment
+rm(list = ls())
+
+
+# Functions ------------------------------------------------------------------------------
+# https://github.com/stas-g/findPeaks
+# a 'peak' is defined as a local maxima with m points either side of it being smaller than it. 
+# hence, the bigger the parameter m, the more stringent is the peak finding procedure
+find_peaks <- function(x, m = 3) {
+  shape <- diff(sign(diff(x, na.pad = FALSE)))
+  pks <- sapply(which(shape < 0), FUN = function(i){
+    z <- i - m + 1
+    z <- ifelse(z > 0, z, 1)
+    w <- i + m + 1
+    w <- ifelse(w < length(x), w, length(x))
+    if(all(x[c(z : i, (i + 2) : w)] <= x[i + 1])) return(i + 1) else return(numeric(0))
+  })
+  pks <- unlist(pks)
+  pks
+}
+
+
+# Load data ------------------------------------------------------------------------------
+# select files to analyze
+flist <- choose.files(caption = "Choose IMTP files")
+
+# load data and header separately
+skip <- list()
+data <- list()
+cols <- c("Time","Z.Left","Z.Right","Left","Right")
+header <- list()
+for (f in flist) {
+  fn <- basename(f)
+  # determine length of header
+  skip[[fn]] <- read.csv(f, nrows = 20, header = F, blank.lines.skip = F)
+  if (any(skip[[fn]] == "Time", na.rm = T)) {
+    skip[[fn]] <- which(skip[[fn]] == 'Time')-1
+    
+    # load data (without header) and keep only Time, Left and Right columns
+    data[[fn]] <- read.csv(f, skip = skip[[fn]])
+    if (any(names(data[[fn]]) %in% cols, na.rm = T)) {
+      data[[fn]] <- data[[fn]][,which(names(data[[fn]]) %in% cols)] # keep only left, right and time columns
+      colnames(data[[fn]]) <- gsub('Z.','',colnames(data[[fn]])) # remove 'Z." from column names
+      
+      data[[fn]]$Total <- data[[fn]]$Left + data[[fn]]$Right
+      
+      # load header
+      header[[fn]] <- read.csv(f, nrows = skip[[fn]]-1, header = F)
+
+    } else {
+      # print warning but continue execution with other files
+      warning(paste("The file ", fn, " does not contain the required data and was not processed"))
+      flist <- flist[-which(flist == f)] # remove the file from file list
+    }
+  } else {
+    # print warning but continue execution with other files
+    warning(paste("The file", fn, "does not contain the required data and was not processed"))
+    flist <- flist[-which(flist == f)] # remove the file from file list
+  }
+}
+rm(skip,cols,f,fn)
+
+fpath <- unique(dirname(flist))
+fnames <- basename(flist)
+
+bodymass <- list()
+freq <- list()
+for (f in fnames) {
+  # find weight for force normalisation
+  if (any(header[[f]] == "Weight", na.rm = T)) {
+    bodymass[[f]] <- as.numeric(na.omit(header[[f]][header[[f]] == 'Weight',2])) * 9.81
+  } else {
+    bodymass[[f]] <- median(data[[f]]$Total)
+  }
+  # find frequency
+  freq[[f]] <- as.numeric(na.omit(header[[f]][header[[f]] == 'Frequency',2]))
+  if (freq[[f]] < 1000) {
+    message(paste(f,": Sampling frequency =",freq[[f]],"\nFrequencies > 1000 Hz are recommended"))
+  }
+}
+rm(f,header)
+
+
+# Determine number of repetition per file ------------------------------------------------
+fmaxi <- list()
+for (f in fnames) {
+  # find peaks (at least 2s apart from each other)
+  d <- freq[[f]]*2 # frequency * 2s
+  pks <- find_peaks(data[[f]]$Total, m = d)
+  plot(x = data[[f]]$Time, y = data[[f]]$Total, type = "l", lwd = 2, 
+       xlab = "Time [s]", ylab = "Force [N]", main = f)
+  points(x = data[[f]]$Time[c(1:nrow(data[[f]]))[pks]], y = data[[f]]$Total[pks], col = "red")
+  # determine a cutoff under which all other peaks cannot be considered IMTP trials
+  cutoff <- (max(data[[f]]$Total[pks])-bodymass[[f]])/2 + bodymass[[f]] # half of BM normalised max force
+  # cutoff <- max(data[[fn]]$Total[pks])-250 # within 250 N of max value
+  abline(h = cutoff, col = "green", lty = "dashed")
+  # only keep peaks above cutoff and more than 5s apart
+  pks <- pks[which(data[[f]]$Total[pks] > cutoff)]
+  d <- freq[[f]]*5 # frequency * 5s
+  if (any(diff(pks) < d)) {
+    pks <- pks[-(which(diff(pks) < d)+1)]
+  }
+  points(x = data[[f]]$Time[c(1:nrow(data[[f]]))[pks]], y = data[[f]]$Total[pks], col = "red", pch = 16)
+
+  
+  # find valleys left and right of peaks to determine start and end of rep
+  for (p in 1:length(pks)) {
+    # define arbitrary period to look for start and end of rep
+    # start of period
+    if (p > 1) { # if not first rep
+      ds <- min(freq[[f]]*5, (pks[p]-pks[p-1])/2) # start of period is 5S before max
+    } else {
+      ds <- min(freq[[f]]*5, pks[p]-1) # else start = either start of trial or 5s before max
+    }
+    
+    # end of period
+    if (p < length(pks)) { # if not last rep
+      de <- min(freq[[f]]*5, (pks[p+1]-pks[p])/2) # end of period = halfway between two adjacent maxes
+    } else {
+      de <- min(freq[[f]]*5, length(data[[f]]$Total)-pks[p]) # else end of period = either end of trial or 5s after max
+    }
+    w = (pks[p]-ds):(pks[p]+de)
+    
+    # look for valleys either side of max
+    vys <- find_peaks(-data[[f]]$Total[w], m = 50)
+    vys <- vys[which(data[[f]]$Total[w[vys]] < cutoff)]
+    
+    # start and end of each rep are valleys closest to max
+    pk <- min(which(data[[f]]$Total[w] == max(data[[f]]$Total[w])))
+    rs <- vys[max(which(vys < pk))]
+    re <- vys[min(which(vys > pk))]
+    
+    # redefine rep
+    # start of period
+    ps <- min(freq[[f]]*1.5, rs-1)
+    # end of period
+    if (p < length(pks)) { # if not last rep
+      pe <- freq[[f]]*1.5 # end of rep = 1.5s after end of pull
+    } else {
+      pe <- min(freq[[f]]*1.5, length(w)-re) # else end of rep = either end of trial or 1.5s after end of pull
+    }
+    w <- w[rs-ps]:w[re+pe]
+    
+    if (length(pks) > 1) {
+      n <- paste(f,p,sep = '_')
+      data[[n]] <- data[[f]][w,]
+      
+      bodymass[[n]] <- bodymass[[f]]
+      freq[[n]] <- freq[[f]]
+      
+      fmaxi[[n]] <- (pks[p] - w[1]) + 1
+      if (p == length(pks)) {
+        data[[f]] <- NULL
+        fmaxi[[f]] <- NULL
+        freq[[f]] <- NULL
+        bodymass[[f]] <- NULL
+        
+      }
+    } else {
+      data[[f]] <- data[[f]][w,]
+      fmaxi[[f]] <- (pks - w[1]) + 1
+    }
+  }
+}
+fmaxi <- fmaxi[match(names(data), names(fmaxi))]
+rm(f,d,pks,cutoff,p,ds,de,w,vys,pk,rs,re,ps,pe,n)
+
+
+# Determine start and end of pull --------------------------------------------------------
+pull <- matrix(0,length(data),2)
+colnames(pull) <- c('start','end')
+for (f in 1:length(data)) {
+  fn <- names(data)[f]
+  # noise = 5 SD of 1 s weighing period before pull
+  # threshold for start of pull = mean force during 1st second + noise
+  spt <- mean(data[[fn]]$Total[1:(freq[[fn]]*1)]) + (sd(data[[fn]]$Total[1:(freq[[fn]]*1)])*5)
+  
+  # start and end of pull = last and first instance when force < start of pull threshold
+  pull[f,1] <- max(which(data[[fn]]$Total[1:fmaxi[[fn]]] < spt))
+  pull[f,2] <- min(which(data[[fn]]$Total[fmaxi[[fn]]:length(data[[fn]]$Total)] < spt)) + fmaxi[[fn]]
+}
+rm(f,fn,spt)
+
+
+# Check for indicators of poor trial -----------------------------------------------------
+
+ptq <- matrix('_',length(data),4)
+colnames(ptq) <- c('Unstable weighing','Unequal forces','Peak force at end of pull','Countermovement')
+rownames(ptq) <- names(data)
+for (f in 1:length(data)) {
+  sp <- pull[f,1]
+  ep <- pull[f,2]
+  bm <- mean(data[[f]]$Total[1:(freq[[f]]*1)])
+  
+  plot(x = data[[f]]$Time, y = data[[f]]$Total, type = "l", lwd = 2, 
+       xlab = "Time [s]", ylab = "Force [N]", main = names(data)[f])
+  abline(h = bm, col = "green", lwd = 2, lty = "dashed")
+  abline(v = data[[f]]$Time[sp], col = "red", lwd = 2, lty = "dotted")
+  abline(v = data[[f]]$Time[ep], col = "red", lwd = 2, lty = "dotted")
+  points(x = data[[f]]$Time[fmaxi[[f]]], y = data[[f]]$Total[fmaxi[[f]]], col = "blue", pch = 8, lwd = 2)
+  
+  # unstable weighing period before pull (change in force > 50 N)
+  if (any(diff(data[[f]]$Total[1:sp]) > 50)) {
+    ptq[f,1] <- 'X'
+    # message(paste("Warning:", names(data)[f], "does not have a stable weighing period"))
+    # # ask whether to continue processing or not
+    # resp <- readline("Would you like to continue processing anyway? (Y/N) ")
+    # if (grepl(resp, 'n', ignore.case = TRUE)) {
+    #   stop("Processing stopped")
+    # }
+  }
+  
+  # comparable force before and after pull
+  pt <- mean(data[[f]]$Total[1:sp]) * 0.1 # threshold = 10% of force before pull
+  if (mean(data[[f]]$Total[1:sp]) > (mean(data[[f]]$Total[ep:length(data[[f]]$Total)]) + pt) ||
+      mean(data[[f]]$Total[1:sp]) < (mean(data[[f]]$Total[ep:length(data[[f]]$Total)]) - pt)) {
+    ptq[f,2] <- 'X'
+    # message(paste("Warning:", names(data)[f], "does not have equal forces before and after the pull"))
+    # # ask whether to continue processing or not
+    # resp <- readline("Would you like to continue processing anyway? (Y/N) ")
+    # if (grepl(resp, 'n', ignore.case = TRUE)) {
+    #   stop("Processing stopped")
+    # }
+  }
+  
+  # peak force during the last 5% of pull
+  if (fmaxi[[f]] >= (sp + (ep-sp)*0.95)) {
+    ptq[f,3] <- 'X'
+    # message(paste("Warning: In", names(data)[f], "peak force occurs at the end of the pull"))
+    # # ask whether to continue processing or not
+    # resp <- readline("Would you like to continue processing anyway? (Y/N) ")
+    # if (grepl(resp, 'n', ignore.case = TRUE)) {
+    #   stop("Processing stopped")
+    # }
+  }
+  
+  # countermovement before pull
+  # CM threshold = mean force of 1st second - 5 SD of force during 1st second
+  cmt <- mean(data[[f]]$Total[1:(freq[[f]]*1)]) - (sd(data[[f]]$Total[1:(freq[[f]]*1)])*5)
+  if (any(data[[f]]$Total[sp-(freq[[f]]*0.5):sp] < cmt)) {
+    ptq[f,4] <- 'X'
+    # message(paste("Warning:", names(data)[f], "contains a countermovement prior to the pull"))
+    # # ask whether to continue processing or not
+    # resp <- readline("Would you like to continue processing anyway? (Y/N) ")
+    # if (grepl(resp, 'n', ignore.case = TRUE)) {
+    #   stop("Processing stopped")
+    # }
+  }
+}
+rm(f,sp,ep,bm,pt,cmt,resp)
+
+
+# Extract performance metrics ------------------------------------------------------------
+# impulse
+# relative peak force
+RFD <-  matrix(0,length(data),5)
+colnames(RFD) <- paste('RFD',seq(50,250,50))
+IMP <-  matrix(0,length(data),5)
+colnames(IMP) <- paste('J-',seq(50,250,50))
+pF <-  matrix(0,length(data),3)
+colnames(pF) <- c('peak force','relative peak force','time to peak force')
+for (f in 1:length(data)) {
+  sp <- pull[f,1]
+  ep <- pull[f,2]
+  # RFD 50, 100, 150, 200 and 250 ms (RFD = change in force / change in time)
+  RFD[f,1] <- (data[[f]]$Total[sp+(freq[[f]]*0.05)] - data[[f]]$Total[sp]) / 0.05
+  RFD[f,2] <- (data[[f]]$Total[sp+(freq[[f]]*0.10)] - data[[f]]$Total[sp]) / 0.10
+  RFD[f,3] <- (data[[f]]$Total[sp+(freq[[f]]*0.15)] - data[[f]]$Total[sp]) / 0.15
+  RFD[f,4] <- (data[[f]]$Total[sp+(freq[[f]]*0.20)] - data[[f]]$Total[sp]) / 0.20
+  RFD[f,5] <- (data[[f]]$Total[sp+(freq[[f]]*0.25)] - data[[f]]$Total[sp]) / 0.25
+  # Impulse from 50, 100, 150, 200 and 250 ms
+  IMP[f,1] <- sum(diff(data[[f]]$Time[sp:(sp+freq[[f]]*0.05)]) * (head(data[[f]]$Total[sp:(sp+freq[[f]]*0.05)],-1) + tail(data[[f]]$Total[sp:(sp+freq[[f]]*0.05)],-1)))/2
+  IMP[f,2] <- sum(diff(data[[f]]$Time[sp:(sp+freq[[f]]*0.10)]) * (head(data[[f]]$Total[sp:(sp+freq[[f]]*0.10)],-1) + tail(data[[f]]$Total[sp:(sp+freq[[f]]*0.10)],-1)))/2
+  IMP[f,3] <- sum(diff(data[[f]]$Time[sp:(sp+freq[[f]]*0.15)]) * (head(data[[f]]$Total[sp:(sp+freq[[f]]*0.15)],-1) + tail(data[[f]]$Total[sp:(sp+freq[[f]]*0.15)],-1)))/2
+  IMP[f,4] <- sum(diff(data[[f]]$Time[sp:(sp+freq[[f]]*0.20)]) * (head(data[[f]]$Total[sp:(sp+freq[[f]]*0.20)],-1) + tail(data[[f]]$Total[sp:(sp+freq[[f]]*0.20)],-1)))/2
+  IMP[f,5] <- sum(diff(data[[f]]$Time[sp:(sp+freq[[f]]*0.25)]) * (head(data[[f]]$Total[sp:(sp+freq[[f]]*0.25)],-1) + tail(data[[f]]$Total[sp:(sp+freq[[f]]*0.25)],-1)))/2
+  # Peak force
+  pF[f,1] <- data[[f]]$Total[fmaxi[[f]]]
+  # Relative peak force
+  pF[f,2] <- pF[f,1]/(bodymass[[f]]/9.81)
+  # Time to peak force
+  pF[f,3] <- (fmaxi[[f]] - sp) / freq[[f]]
+}
+rm(f,sp,ep)
+
+
+# Export results -------------------------------------------------------------------------
+ex <- readline("Do you want to export the results? (Y/N) ")
+if (grepl(ex,'y',ignore.case = T)) {
+  # tbl <- cbind(pF,RFD,IMP)
+  tbl <- cbind(pF,RFD,IMP,ptq)
+  rownames(tbl) <- names(data)
+  # file name
+  fn <- readline("Enter the name of the file you want to save the results to: ")
+  # file path
+  fp <- dirname(flist[1])
+  
+  # write.xlsx(tbl, file = paste0(fp,'/',fn,'.xlsx'))
+  write.csv(tbl, file = paste0(fp,'/',fn,'.csv'))
+  rm(tbl)
+}
+
---
+++ IMTP_app/IMTP_functions.R
@@ -0,0 +1,245 @@
+#-----------------------------------------------------------------------------------------
+#
+# Functions for IMTP analysis Shinny app
+# Robert Schuster (ACU SPRINT)
+# August 2022
+#
+#-----------------------------------------------------------------------------------------
+
+# Find peaks -----------------------------------------------------------------------------
+# https://github.com/stas-g/findPeaks
+# a 'peak' is defined as a local maxima with m points either side of it being smaller than it. 
+# hence, the bigger the parameter m, the more stringent is the peak finding procedure
+find_peaks <- function(x, m = 3) {
+  shape <- diff(sign(diff(x, na.pad = FALSE)))
+  pks <- sapply(which(shape < 0), FUN = function(i){
+    z <- i - m + 1
+    z <- ifelse(z > 0, z, 1)
+    w <- i + m + 1
+    w <- ifelse(w < length(x), w, length(x))
+    if(all(x[c(z : i, (i + 2) : w)] <= x[i + 1])) return(i + 1) else return(numeric(0))
+  })
+  pks <- unlist(pks)
+  pks
+}
+
+
+# Load data ------------------------------------------------------------------------------
+importTrial <- function(filepath,filename) {
+  cols <- c("Time","Z.Left","Z.Right","Left","Right")
+  fn <- basename(filename)
+  # determine length of header
+  skip <- read.csv(filepath, nrows = 20, header = F, blank.lines.skip = F)
+  if (any(skip == "Time", na.rm = T)) {
+    skip <- which(skip == 'Time')-1
+    
+    # load data (without header) and keep only Time, Left and Right columns
+    df <- read.csv(filepath, skip = skip)
+    if (any(names(df) %in% cols, na.rm = T)) {
+      df <- df[,which(names(df) %in% cols)] # keep only left, right and time columns
+      colnames(df) <- gsub('Z.','',colnames(df)) # remove 'Z." from column names
+      
+      df$Total <- df$Left + df$Right
+      
+      # load header
+      header <- read.csv(filepath, nrows = skip-1, header = F)
+      # find weight for force normalisation
+      if (any(header == "Weight", na.rm = T)) {
+        bodymass <- as.numeric(na.omit(header[header == 'Weight',2])) * 9.81
+      } else {
+        bodymass <- median(df$Total)
+      }
+      # find frequency
+      freq <- as.numeric(na.omit(header[header == 'Frequency',2]))
+      if (freq < 1000) {
+        message(paste(f,": Sampling frequency =",freq,"\nFrequencies > 1000 Hz are recommended"))
+      }
+      
+      data <- list("df" = df, "bodymass" = bodymass, "freq" = freq, "fn" = fn)
+      
+      return(data)
+      
+    } else {
+      # print warning but continue execution with other files
+      warning(paste("The file ", fn, " does not contain the required data and was not processed"))
+    }
+  } else {
+    # print warning but continue execution with other files
+    warning(paste("The file", fn, "does not contain the required data and was not processed"))
+  }
+}
+
+
+# Determine number of repetitions --------------------------------------------------------
+nReps <- function(data) {
+  # find peaks (at least 2s apart from each other)
+  d <- data$freq*2 # frequency * 2s
+  pks <- find_peaks(data$df$Total, m = d)
+  # determine a cutoff under which all other peaks cannot be considered IMTP trials
+  cutoff <- (max(data$df$Total[pks])-data$bodymass)/2 + data$bodymass # half of BM normalised max force
+  # cutoff <- max(data$df$Total[pks])-250 # within 250 N of max value
+  # only keep peaks above cutoff and more than 5s apart
+  pks <- pks[which(data$df$Total[pks] > cutoff)]
+  d <- data$freq*5 # frequency * 5s
+  if (any(diff(pks) < d)) {
+    pks <- pks[-(which(diff(pks) < d)+1)]
+  }
+  
+  # If trial contains more than one rep, cut trial into separate reps
+  # find valleys left and right of peaks to determine start and end of rep
+  for (p in 1:length(pks)) {
+    # define arbitrary period to look for start and end of rep
+    # start of period
+    if (p > 1) { # if not first rep
+      ds <- min(data$freq*5, (pks[p]-pks[p-1])/2) # start of period is 5S before max
+    } else {
+      ds <- min(data$freq*5, pks[p]) # else start = either start of trial or 5s before max
+    }
+    # end of period
+    if (p < length(pks)) { # if not last rep
+      de <- min(data$freq*5, (pks[p+1]-pks[p])/2) # end of period = halfway between two adjacent maxes
+    } else {
+      de <- min(data$freq*5, length(data$df$Total)-pks[p]) # else end of period = either end of trial or 5s after max
+    }
+    w = (pks[p]-ds):(pks[p]+de)
+    
+    # look for valleys either side of max
+    vys <- find_peaks(-data$df$Total[w], m = 50)
+    vys <- vys[which(data$df$Total[w[vys]] < cutoff)]
+    
+    # start and end of each rep are valleys closest to max
+    pk <- min(which(data$df$Total[w] == max(data$df$Total[w])))
+    rs <- vys[max(which(vys < pk))]
+    re <- vys[min(which(vys > pk))]
+    
+    # redefine rep
+    # start of rep
+    ps <- min(data$freq*1.5, rs-1)
+    # end of rep
+    if (p < length(pks)) { # if not last rep
+      pe <- data$freq*1.5 # end of rep = 1.5s after end of pull
+    } else {
+      pe <- min(data$freq*1.5, length(w)-re) # else end of rep = either end of trial or 1.5s after end of pull
+    }
+    w <- w[rs-ps]:w[re+pe]
+    
+    if (length(pks) > 1) {
+      n <- paste(data$fn,'_',p)
+      data[[n]] <- data$df[w,]
+      
+      data$fmaxi[p] <- (pks[p] - w[1]) + 1
+      
+      if (p == length(pks)) {
+        data$df <- NULL
+      }
+    } else {
+      data[[data$fn]] <- data$df[w,]
+      data$fmaxi <- (pks - w[1]) + 1
+      
+      data$df <- NULL
+    }
+  }
+  return(data)
+}
+
+
+# Determine start and end of pull --------------------------------------------------------
+pull <- function(data) {
+  reps <- names(data)[which(grepl(data$fn,names(data)))]
+  pull <- matrix(0,length(reps),2)
+  colnames(pull) <- c('start','end')
+  
+  for (r in 1:length(reps)) {
+    rn <- reps[r]
+    # noise = 5 SD of 1 s weighing period before pull
+    # threshold for start of pull = mean force during 1st second + noise
+    spt <- mean(data[[rn]]$Total[1:(data$freq*1)]) + (sd(data[[rn]]$Total[1:(data$freq*1)])*5)
+    
+    # start and end of pull = last and first instance when force < start of pull threshold
+    pull[r,1] <- max(which(data[[rn]]$Total[1:data$fmaxi[r]] < spt))
+    pull[r,2] <- min(which(data[[rn]]$Total[data$fmaxi[r]:length(data[[rn]]$Total)] < spt)) + data$fmaxi[r]
+  }
+  data$pull <- pull
+  return(data)
+}
+
+
+# Check for indicators of poor trial -----------------------------------------------------
+qualityCheck <- function(data) {
+  reps <- names(data)[which(grepl(data$fn,names(data)))]
+  data$warn <- list()
+  for (r in 1:length(reps)) {
+    rn <- reps[r]
+    sp <- data$pull[r,1]
+    ep <- data$pull[r,2]
+    bm <- mean(data[[rn]]$Total[1:(data$freq*1)])
+    i <- 1
+    
+    # unstable weighing period before pull (change in force > 50 N)
+    if (any(diff(data[[rn]]$Total[1:sp]) > 50)) {
+      data$warn[[rn]][[i]] <- "This rep does not have a stable weighing period"
+      i <- i+1
+    }
+    
+    # comparable force before and after pull
+    pt <- mean(data[[rn]]$Total[1:sp]) * 0.1 # threshold = 10% of force before pull
+    if (mean(data[[rn]]$Total[1:sp]) > (mean(data[[rn]]$Total[ep:length(data[[rn]]$Total)]) + pt) ||
+        mean(data[[rn]]$Total[1:sp]) < (mean(data[[rn]]$Total[ep:length(data[[rn]]$Total)]) - pt)) {
+      data$warn[[rn]][[i]] <- "This rep does not have equal forces before and after the pull"
+      i <- i+1
+    }
+    
+    # peak force during the last 5% of pull
+    if (data$fmaxi[r] >= (sp + (ep-sp)*0.95)) {
+      data$warn[[rn]][[i]] <- "In this rep peak force occurs at the end of the pull"
+      i <- i+1
+    }
+    
+    # countermovement before pull
+    # CM threshold = mean force of 1st second - 5 SD of force during 1st second
+    cmt <- mean(data[[rn]]$Total[1:(data$freq*1)]) - (sd(data[[rn]]$Total[1:(data$freq*1)])*5)
+    if (any(data[[rn]]$Total[sp-(data$freq*0.5):sp] < cmt)) {
+      data$warn[[rn]][[i]] <- "This rep contains a countermovement prior to the pull"
+      i <- i+1
+    }
+  }
+  return(data)
+}
+
+
+# Performance metrics --------------------------------------------------------------------
+perfMetrics <- function(data) {
+  reps <- names(data)[which(grepl(data$fn,names(data)))]
+  pm <- as.data.frame(matrix(0, length(reps),13))
+  colnames(pm) <- c('peak force', 'relative peak force', 'time to peak force', paste('RFD',seq(50,250,50)), 
+                    paste('J-',seq(50,250,50)))
+  rownames(pm) <- reps
+  
+  for (r in 1:length(reps)) {
+    rn <- reps[r]
+    sp <- data$pull[r,1]
+    ep <- data$pull[r,2]
+    # Peak force
+    pm[r,1] <- data[[rn]]$Total[data$fmaxi[r]]
+    # Relative peak force
+    pm[r,2] <- pm[r,1]/(data$bodymass/9.81)
+    # Time to peak force
+    pm[r,3] <- (data$fmaxi[r] - sp) / data$freq
+    # RFD 50, 100, 150, 200 and 250 ms (RFD = change in force / change in time)
+    pm[r,4] <- (data[[rn]]$Total[sp+(data$freq*0.05)] - data[[rn]]$Total[sp]) / 0.05
+    pm[r,5] <- (data[[rn]]$Total[sp+(data$freq*0.10)] - data[[rn]]$Total[sp]) / 0.10
+    pm[r,6] <- (data[[rn]]$Total[sp+(data$freq*0.15)] - data[[rn]]$Total[sp]) / 0.15
+    pm[r,7] <- (data[[rn]]$Total[sp+(data$freq*0.20)] - data[[rn]]$Total[sp]) / 0.20
+    pm[r,8] <- (data[[rn]]$Total[sp+(data$freq*0.25)] - data[[rn]]$Total[sp]) / 0.25
+    # Impulse after 50, 100, 150, 200 and 250 ms
+    pm[r,9] <- sum(diff(data[[rn]]$Time[sp:(sp+data$freq*0.05)]) * (head(data[[rn]]$Total[sp:(sp+data$freq*0.05)],-1) + tail(data[[rn]]$Total[sp:(sp+data$freq*0.05)],-1)))/2
+    pm[r,10] <- sum(diff(data[[rn]]$Time[sp:(sp+data$freq*0.10)]) * (head(data[[rn]]$Total[sp:(sp+data$freq*0.10)],-1) + tail(data[[rn]]$Total[sp:(sp+data$freq*0.10)],-1)))/2
+    pm[r,11] <- sum(diff(data[[rn]]$Time[sp:(sp+data$freq*0.15)]) * (head(data[[rn]]$Total[sp:(sp+data$freq*0.15)],-1) + tail(data[[rn]]$Total[sp:(sp+data$freq*0.15)],-1)))/2
+    pm[r,12] <- sum(diff(data[[rn]]$Time[sp:(sp+data$freq*0.20)]) * (head(data[[rn]]$Total[sp:(sp+data$freq*0.20)],-1) + tail(data[[rn]]$Total[sp:(sp+data$freq*0.10)],-1)))/2
+    pm[r,13] <- sum(diff(data[[rn]]$Time[sp:(sp+data$freq*0.25)]) * (head(data[[rn]]$Total[sp:(sp+data$freq*0.25)],-1) + tail(data[[rn]]$Total[sp:(sp+data$freq*0.25)],-1)))/2
+  }
+  # data$pm <- data.frame(pm)
+  data$pm <- round(pm[,1:13],2)
+  return(data)
+}
+
